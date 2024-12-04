088e0b8c14a8d5c2b3a73b3658164a3d8cc47151
AbangAyoma <abangayoma@gmail.com>
2024-12-04 13:01:16 +0100

    connecting to server error display

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,19 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('connect', () => {
+    console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+(async () => {
+    try {
+        await client.connect();
+    } catch (err) {
+        console.log(`Failed to connect: ${err.message}`);
+    }
+})();
